990ff3ecd84a6d9eb0d967115777e91eeaec5eeb
MakG-Go <tribotrib@gmail.com>
2026-07-23 17:58:43 +0300

    [FIX] Общая сумма столешниц

--- src/components/Basket/helper/basketMapper.ts
+++ src/components/Basket/helper/basketMapper.ts
@@ -627,6 +627,7 @@ function createDefaultTableTopData(filteredData: TTotalProps) {
 
   const { getGlobalOptions } = useRoomOptions()
   const tableTopId = getGlobalOptions?.tableTop?.id
+  const _PRODUCTS = appDataStore.getAppData.CATALOG.PRODUCTS
 
   if (tableTopId === emptyTableTopId) return false
 
